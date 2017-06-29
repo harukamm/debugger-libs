08b265b800b4389607e06a994deb23aa5bd5001c
Haruka Matsumoto <harukam0416@gmail.com>
2017-06-29 18:38:32 +0900

    Value and type for lambda expression
    This represents a temporary value/type for lambda. We'd like to determine the type
    of a lambda based on its context.

---
+++ Mono.Debugger.Soft/Mono.Debugger.Soft/DelayedLambdaValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Mono.Debugger.Soft
+{
+	public class DelayedLambdaValue : Value
+	{
+		DelayedLambdaType delayedType;
+
+		public DelayedLambdaValue (VirtualMachine vm, DelayedLambdaType delayedType) : base (vm, 0)
+		{
+			this.delayedType = delayedType;
+		}
+
+		public string Expression {
+			get { return DelayedType.Expression; }
+		}
+
+		public DelayedLambdaType DelayedType {
+			get { return delayedType; }
+		}
+
+		public string GetLiteralType (TypeMirror t)
+		{
+			return delayedType.GetLiteralType (t);
+		}
+
+		public bool IsAcceptableType (TypeMirror toType)
+		{
+			return delayedType.IsAcceptableType (toType);
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("LambdaValue for ({0})", Expression);
+		}
+	}
+}
